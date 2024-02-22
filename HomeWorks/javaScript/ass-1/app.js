f901eeae089861dd3726e30e2344a2c76aabf5ad
Mahir <mkojak75@gmail.com>
2024-02-22 19:43:32 +0100

    bootstrap

---
+++ HomeWorks/javaScript/ass-1/app.js
@@ -0,0 +1,72 @@
+"use strict "
+
+
+title.textContent = "Javascript DOM Assignment 1"
+
+document.querySelector("header").style.backgroundColor = "yellow"
+document.querySelector("header").style.padding = "10px"
+document.querySelector("header").style.color = "blue"
+document.querySelector("header").style.textAlign ="center"
+document.querySelector("header").style.fontSize = "1.5rem"
+
+
+
+console.log(document.querySelector(".nav-item"));
+document.querySelector(".nav-item").style.display = "Flex";
+document.querySelector(".nav-item").style.justifyContent = "center";
+document.querySelector(".nav-item").style.gap = "2rem"
+document.querySelector(".nav-item").style.listStyle = "none"
+document.querySelector(".nav-item").style.fontSize = "1.5rem"
+
+
+console.log(document.querySelector("#input"));
+document.querySelector("#input").style.backgroundColor = "red"
+document.querySelector("#input").style.padding = "12px"
+document.querySelector("#input").style.borderRadius =" 10px"
+
+
+
+console.log(document.querySelector("#username")); 
+document.querySelector("#username").value = "Anthony"
+document.querySelector("#username").style.padding = "10px"
+document.querySelector("#username").style.borderRadius = "8px"
+
+
+console.log(document.querySelector("#password"));
+document.querySelector("#password").value = "123456"
+document.querySelector("#password").style.padding = "10px"
+document.querySelector("#password").style.borderRadius =" 8px"
+
+
+console.log(document.querySelector(".btn"));
+document.querySelector(".btn").textContent = "Giris Yap"
+document.querySelector(".btn").style.backgroundColor = "green"
+document.querySelector(".btn").style.padding = "12px"
+document.querySelector(".btn").style.color = "white"
+document.querySelector(".btn").style.textAlign ="center"
+document.querySelector(".btn").style.border ="none"
+document.querySelector(".btn").style.borderRadius =" 10px"
+
+
+console.log(document.getElementById("projects"));
+document.getElementById("projects").innerText = "Js Dom Projects"
+document.getElementById("projects").style.backgroundColor = "red"
+document.getElementById("projects").style.fontSize = "2rem"
+
+
+console.log(document.getElementsByTagName("ul"));
+
+
+const myProjects = ["Hello World!","Guess Number","Checkout Page","Gelir-Gider Projesi","Api Projects"];
+console.log(myProjects);
+document.querySelector
+
+
+
+
+
+
+
+
+
+
--- JavaSript/Lektion-12/createNode.js
+++ JavaSript/Lektion-12/createNode.js
@@ -26,7 +26,48 @@ react.before(newLi, newLi2)
 // ID, CLASS GIBI ÖZELLIKLERI ATAMA
 
 //? element.attribute = "yeni deger"
-newLi.className = "list"
+//yazma 
+newLi.className = "list bold text-success bg-warning"
 newLi.id = "new-li"
-newLi.type = "button"
+// newLi.type = "button"
 newLi.name ="list"
+
+//* okuma
+console.log(newLi.className)
+
+//? 2- setAttribute(atr,"deger") getAttrbute("atr")
+
+newLi2.setAttribute("class", "list red bold")
+
+console.log(newLi2.getAttribute("id"))
+console.log(newLi2.getAttribute("class"))
+
+//? 3) Yontem (classList)
+newLi.classList.add("border", "border-danger", "border-3")
+
+newLi.classList.remove("bold", "text-success")
+
+console.log(newLi2.classList.contains("red"))
+
+newLi.classList.toggle("red")
+newLi2.classList.toggle("red")
+
+
+//! yeni bir element olusturma (innerHTML)
+
+
+document.querySelector("main").innerHTML += `
+<div class="d-flex gap-2">
+    <input type="text" class="form-control"/>
+    <button class="btn btn-danger">Submit</button>
+</div>
+
+<div class="card mt-4" style="width: 13rem;">
+    <img src='https://images.pexels.com/photos/19789774/pexels-photo-19789774/free-photo-of-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=300' class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
+`
