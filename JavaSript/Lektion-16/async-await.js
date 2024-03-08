0c5f5188245b592f0a396b2184f73ab7d7722169
Mahir <mkojak75@gmail.com>
2024-03-08 10:53:28 +0100

    async finish

--- JavaSript/Lektion-16/async-await.js
+++ JavaSript/Lektion-16/async-await.js
@@ -18,3 +18,46 @@
 //* satirdaki kodun durdurulmasini saglar.
 //* Yapilan istek yerine getirilip sonuc degerlerinin dondurulmesi ile kodun calismasi devam eder.
 
+const getNews = async () => {
+  //?   https://newsapi.org/v2/top-headlines?country=tr&apiKey=1a1a999e0d7240a6bd2dead87bcca78e&category=technology
+
+  const BASE_URL = `https://newsapi.org/v2/`;
+  const API_KEY = `f05fcb0355984e119134fdad9b47c07f`;
+  const queryString = `top-headlines?country=us&category=technology`;
+  const URL = `${BASE_URL}${queryString}&apiKey=${API_KEY}`;
+
+  try {
+    const res = await fetch(URL);
+
+    //? Error handing
+    if (!res.ok) {
+      throw new Error(`${res.status}`);
+    }
+    const data = await res.json();
+    dipslayNews(data.articles);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const dipslayNews = (news) => {
+    const newsArticle = document.getElementById("news")
+    const defaultImage = "./img/404.png"
+  
+    news.forEach(({ urlToImage, url, content, title }) => {
+      newsArticle.innerHTML += `
+          <div class="card col-sm-6 col-md-4 col-lg-3">
+              <img src="${
+                urlToImage || defaultImage
+              }" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">${title}</h5>
+                  <p class="card-text">${content || "No Content"}</p>
+                  <a href="${url}" target="_blank" class="btn btn-primary">Detail</a>
+              </div>
+          </div>
+      `
+    })
+  }
+
+getNews();
