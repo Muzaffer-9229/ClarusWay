741eadd480f1febab4f852286176487de340e8cb
Mahir <mkojak75@gmail.com>
2024-03-12 11:09:09 +0100

    korrektur

--- JavaSript/Lektion-19/vite/main.js
+++ JavaSript/Lektion-19/vite/main.js
@@ -1,7 +1,3 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
 import { getUsers } from "./src/getUsers"
 import "./style.css"
